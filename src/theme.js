624cfd87ba2ae31572a2661ed27609ff5d628f52
MaxwellBrohm <160036477+MaxwellBrohm@users.noreply.github.com>
2024-05-22 21:44:20 +0000

    color

--- src/theme.js
+++ src/theme.js
@@ -9,7 +9,7 @@ const theme = {
     contrast: 'yellow',
     cubeEmissionEdgeColor: { r: 0, g: 1, b: 1 }, // values should be from 0-1
     cubeEdgeColor: { r: 0, g: 1, b: 1 }, // values should be from 0-1
-    cubeEmissionFaceColor: { r: 0, g: 0.05, b: 0.05 }, // values should be from 0-1
+    cubeEmissionFaceColor: { r: 1, g: 0.7, b: 0 }, // values should be from 0-1
     cubeFaceColor: { r: 0, g: 0, b: 0 },
   },
 };
